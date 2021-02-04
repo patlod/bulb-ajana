f6555371f3924a2f6bf81af0728fc66364b50e4e
yx20awin <patrick.lodes123@gmail.com>
2021-02-04 03:07:28 +0100

    removed file tree.

--- file-tree.js
+++
@@ -1,7 +0,0 @@
-$('.ui.accordion').accordion({ exclusive: false });
-$('.ui.buttons .button').on('click', function() {
-  $(this).addClass('positive')
-         .siblings()
-         .removeClass('positive');
-  $('.treemenu').toggleClass('boxed');
-});
